12e71959e078bf8e9ba99f40462bd2ec579edfc9
MNumaan <136721365+MNumaan@users.noreply.github.com>
2026-02-21 23:57:39 +0500

    Refactor deepcopier to use for-each loop

--- deep_copy_list.py
+++ deep_copy_list.py
@@ -1,7 +1,7 @@
 def deepcopier(userlist):
     copy_list=[]
-    for i in range(len(userlist)):
-        if isinstance(userlist[i],list):
-            copy_list.append(deepcopier(userlist[i]*1))
-        else: copy_list.append(userlist[i]*1)
+    for i in userlist:
+        if isinstance(i,list):
+            copy_list.append(deepcopier(i*1))
+        else: copy_list.append(i*1)
     return copy_list
